a7536939d496c41426f55426e42b67e3816e1ad6
Chung Thế Hào <chungthehao@gmail.com>
2019-10-06 21:46:31 +0700

    Create & Respond With JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
@@ -35,7 +37,22 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt)
 
         await user.save()
-        res.send(user)
+        
+        // Send token back
+        const payload = {
+            user: { id: user.id }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err
+
+                res.json({ token })
+            }
+        )
     } catch (err) {
         console.error(err.message)
         res.status(500).send('Server error.')
